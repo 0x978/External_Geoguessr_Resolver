5ef6fc58142b70abcf3be077afc23e495938b4e7
Logan <cb58logan@gmail.com>
2025-08-09 22:18:10 +0100

    Fetch location info

--- Website/geoviewer/pages/index.tsx
+++ Website/geoviewer/pages/index.tsx
@@ -7,15 +7,28 @@ type Coords = {
   sessionId: string;
 };
 
+type location = {
+    country: string;
+    town: string;
+    village: string;
+    state: string;
+    road: string;
+    county: string;
+    postcode: string;
+    city: string;
+}
+
 export default function Home() {
   const [sessionId, setSessionId] = useState<string>('');
   const [coords, setCoords] = useState<Coords | null>(null);
+  const [resolvedLocation, setResolvedLocation] = useState<location | null>(null);
   const wsRef = useRef<WebSocket | null>(null);
 
   useEffect(() => {
     if (!sessionId) return;
 
     const socket = new WebSocket(`wss://${location.host}/ws/${sessionId}`);
+    // const socket = new WebSocket(`ws://localhost:8000/ws/${sessionId}`); // Local dev
     wsRef.current = socket;
 
     socket.onopen = () => console.log('connected');
@@ -24,6 +37,7 @@ export default function Home() {
       try {
         const data: Coords = JSON.parse(e.data);
         setCoords(data);
+        setLocationFromCoords(data.lat, data.lng);
       } catch (err) {
         console.error('bad data', err);
       }
@@ -34,6 +48,29 @@ export default function Home() {
 
   function openInMaps() {
     window.open(`https://maps.google.com/?output=embed&q=${coords?.lat},${coords?.lng}&ll=${coords?.lat},${coords?.lng}&z=5`)
+  }
+
+  async function setLocationFromCoords(lat: number, lon: number): Promise<void> {
+    const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`;
+    const res = await fetch(url, {
+      headers: {
+          'User-Agent': 'GeoResolver',
+          'Accept-Language': 'en'
+      }
+    });
+    const data = await res.json();
+    const { city, town, village, state, country, road, county, postcode } = data.address;
+    const location: location = {
+      city: city,
+      town: town,
+      village: village,
+      state: state,
+      road: road,
+      county: county,
+      postcode: postcode,
+      country: country,
+    };
+    setResolvedLocation(location)
   }
 
   return (
@@ -50,10 +87,24 @@ export default function Home() {
               <>
                 <div><b>Lat:</b> {coords.lat}</div>
                 <div><b>Lng:</b> {coords.lng}</div>
-                <div><b>Time:</b> {new Date(coords.timestamp * 1000).toLocaleTimeString()}</div>
+                <div><b>Time:</b> {new Date(coords.timestamp * 1000).toLocaleTimeString()}
+                { resolvedLocation &&
+                    <>
+                        <div><b>country:</b> {resolvedLocation.country}</div>
+                        <div><b>state:</b> {resolvedLocation.state}</div>
+                        <div><b>county:</b> {resolvedLocation.county}</div>
+                        <div><b>city:</b> {resolvedLocation.city}</div>
+                        <div><b>town:</b> {resolvedLocation.town}</div>
+                        <div><b>village:</b> {resolvedLocation.village}</div>
+                        <div><b>road:</b> {resolvedLocation.road}</div>
+                        <div><b>postcode:</b> {resolvedLocation.postcode}</div>
+                    </>
+                }
+                </div>
               </>
           ) : (
-              <div>No coordinates received yet.</div>
+              <div>No coordinates received yet.
+              </div>
           )}
         </div>
         <button className="w-64 h-16 rounded-xl bg-gray-300 text-black cursor-pointer" onClick={() => openInMaps()} >Google Maps</button>
